3b63570b8245785a38be5b7e9f5f51186d963cf1
erolabs <142560626+erolabs@users.noreply.github.com>
2023-09-08 22:25:04 +0530

    Added Sql Scripts

---
+++ SqlDbEditor/SqlScripts/GetCustomers.sql
@@ -0,0 +1,18 @@
+﻿USE [LinkTekTest]
+GO
+/****** Object:  StoredProcedure [dbo].[GetCustomers]    Script Date: 08-09-2023 22:22:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER PROCEDURE [dbo].[GetCustomers]
+    @Offset INT,
+    @PageSize INT
+AS
+BEGIN
+    SELECT CustomerId, FirstName, LastName, Address1, Address2, City, State, Zip, Phone, Age, Sales, CreatedTime, UpdatedTime 
+	FROM dbo.Customer 
+	ORDER BY CustomerId OFFSET @Offset 
+	ROWS FETCH NEXT @PageSize ROWS ONLY
+END
